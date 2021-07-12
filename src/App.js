1d102bac4b625804a406cddef3e57ed371d3328f
Hyunjun Yoon <yhj940719@gmail.com>
2021-07-12 18:29:44 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages';
+import About from './pages/about';
+import Menu from './pages/menu';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    console.log(isOpen);
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
   return (
     <>
-     <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch></Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/menu' exact component={Menu} />
+      <Route path='/about' exact component={About} />
+      <Footer />
     </>
   );
 }
